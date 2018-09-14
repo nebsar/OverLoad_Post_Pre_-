9af9d22a448c9a332075768420af064e4f5e7e8b
Eagleye <Eagleye@EagleEye>
2018-09-14 03:46:42 -0400

    OverLoad_Post_Pre_++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: Eagleye
@@ -11,14 +5,39 @@
  * Created on September 14, 2018, 3:34 AM
  */
 
-#include <cstdlib>
+#include <iostream>
 
-using namespace std;
+struct Int {
+    int m_val;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+    Int(int val) : m_val(val) {
+    }
+
+    Int& operator++() { // pre-increment
+        m_val++;
+        return *this;
+    }
+
+    Int operator++(int dummy) {//post increment
+        Int tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+};
+
+int main() {
+
+    Int b = 100;
+
+    Int z = b++;
+
+    std::cout << z.m_val << '\n';
+
+    b=100;
+
+    z = ++b;
+
+    std::cout << z.m_val << '\n';
 
     return 0;
 }
